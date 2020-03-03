62d773cf43ce054e0bf63940009425efeb4d2ac7
amendoa <almeida-matheus@hotmail.com.br>
2020-03-03 11:27:30 -0300

    fix: undefined window

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,221 +1,233 @@
-import $ from 'jquery'
-import emitter from '../utils/emitter'
+import $ from "jquery";
+import emitter from "../utils/emitter";
 
-const NAME = 'modal'
-const HASH = '#modal-open'
+const NAME = "modal";
+const HASH = "#modal-open";
 
 const templates = {
   modal: '<div class="modal"></div>',
   close: '<i class="modal-close glyph glyph-x"></i>',
   content: '<div class="modal-content"><div class="modal-body"></div></div>'
-}
+};
 
 const DEFAULTS = {
-  container: 'body',
-  size: 'medium',
+  container: "body",
+  size: "medium",
   triggerClose: null,
   static: false,
   keyboard: true,
   history: false,
   keys: { esc: 27 },
   triggerOpen: null
-}
+};
 
 class Modal {
-  constructor (element, options) {
-    this.$element = (element instanceof $) ? element : $(element)
-    this.options = $.extend({}, DEFAULTS, (options || {}))
+  constructor(element, options) {
+    this.$element = element instanceof $ ? element : $(element);
+    this.options = $.extend({}, DEFAULTS, options || {});
   }
 
-  init () {
-    this.$container = $(this.options.container)
-    this.createModal()
+  init() {
+    this.$container = $(this.options.container);
+    this.createModal();
 
-    return this
+    return this;
   }
 
-  show () {
-    this.bindListeners()
-    this.showModal()
+  show() {
+    this.bindListeners();
+    this.showModal();
   }
 
-  hide () {
-    this.unbindListeners()
-    this.hideModal()
+  hide() {
+    this.unbindListeners();
+    this.hideModal();
   }
 
-  destroy () {
-    this.$element.removeData(NAME)
-    this.$modal.remove()
+  destroy() {
+    this.$element.removeData(NAME);
+    this.$modal.remove();
   }
 
-  bindListeners () {
+  bindListeners() {
     if (this.options.triggerClose) {
-      this.$modal.on('click', this.options.triggerClose, this.hide.bind(this))
+      this.$modal.on("click", this.options.triggerClose, this.hide.bind(this));
     }
 
-    this.$close.on('click', this.hide.bind(this))
+    this.$close.on("click", this.hide.bind(this));
 
-    this.bindKeyboardListener()
+    this.bindKeyboardListener();
 
     if (this.options.history) {
-      this.bindModalShowListener()
-      this.bindHashChangeListener()
-      this.bindModalHideListener()
+      this.bindModalShowListener();
+      this.bindHashChangeListener();
+      this.bindModalHideListener();
     }
   }
 
-  bindKeyboardListener () {
+  bindKeyboardListener() {
     if (this.options.keyboard) {
-      $(window).on('keyup', (e) => {
-        const { esc } = this.options.keys
-        const key = e.which
+      if (typeof window != "undefined") {
+        $(window).on("keyup", e => {
+          const { esc } = this.options.keys;
+          const key = e.which;
 
-        if (key === esc) {
-          this.hide()
+          if (key === esc) {
+            this.hide();
+          }
+        });
+      }
+    }
+  }
+
+  bindModalShowListener() {
+    emitter.on("modal:show", () => {
+      if (typeof window != "undefined") {
+        window.location.hash = HASH;
+      }
+    });
+  }
+
+  bindHashChangeListener() {
+    if (typeof window != "undefined") {
+      $(window).on("hashchange", () => {
+        if (window.location.hash) {
+          return;
         }
-      })
+
+        this.hide();
+      });
     }
   }
 
-  bindModalShowListener () {
-    emitter.on('modal:show', () => {
-      window.location.hash = HASH
-    })
-  }
-
-  bindHashChangeListener () {
-    $(window).on('hashchange', () => {
-      if (window.location.hash) {
-        return
+  bindModalHideListener() {
+    emitter.on("modal:hide", () => {
+      if (typeof window != "undefined") {
+        if (window.location.hash) {
+          return history.back();
+        }
       }
-
-      this.hide()
-    })
+    });
   }
 
-  bindModalHideListener () {
-    emitter.on('modal:hide', () => {
-      if (window.location.hash) {
-        return history.back()
-      }
-    })
-  }
-
-  unbindListeners () {
+  unbindListeners() {
     if (this.options.triggerClose) {
-      this.$modal.off('click', this.options.triggerClose, this.hide.bind(this))
+      this.$modal.off("click", this.options.triggerClose, this.hide.bind(this));
     }
 
-    this.$close.off('click')
-    $(window).off('keyup', this.handler)
+    this.$close.off("click");
+    if (typeof window != "undefined") {
+      $(window).off("keyup", this.handler);
+    }
   }
 
-  bindTrigger () {
+  bindTrigger() {
     if (this.options.triggerOpen) {
       $(this.options.triggerOpen).on(
-        'click',
+        "click",
         this.onTriggerOpenClick.bind(this)
-      )
+      );
     }
   }
 
-  onTriggerOpenClick (event) {
-    event.preventDefault()
-    this.show()
+  onTriggerOpenClick(event) {
+    event.preventDefault();
+    this.show();
   }
 
-  showModal () {
-    emitter.emit('modal:show')
-    emitter.removeAllListeners('modal:show')
+  showModal() {
+    emitter.emit("modal:show");
+    emitter.removeAllListeners("modal:show");
 
-    this.$modal.addClass('modal-enter')
-    this.$content.addClass('modal-content-enter')
-    this.$container.addClass('no-scroll')
+    this.$modal.addClass("modal-enter");
+    this.$content.addClass("modal-content-enter");
+    this.$container.addClass("no-scroll");
 
-    window.setTimeout(() => {
-      this.$modal.addClass('modal-show')
-      this.$content.addClass('modal-content-show')
-    }, 200)
+    if (typeof window != "undefined") {
+      window.setTimeout(() => {
+        this.$modal.addClass("modal-show");
+        this.$content.addClass("modal-content-show");
+      }, 200);
+    }
 
-    this.$modal.on('click', this.onModalClick.bind(this))
+    this.$modal.on("click", this.onModalClick.bind(this));
   }
 
-  hideModal () {
-    emitter.emit('modal:hide')
-    emitter.removeAllListeners('modal:hide')
+  hideModal() {
+    emitter.emit("modal:hide");
+    emitter.removeAllListeners("modal:hide");
 
     this.$content
-      .removeClass('modal-content-show')
-      .addClass('modal-content-leave')
+      .removeClass("modal-content-show")
+      .addClass("modal-content-leave");
 
-    this.$modal
-      .removeClass('modal-show')
-      .addClass('modal-leave')
+    this.$modal.removeClass("modal-show").addClass("modal-leave");
 
-    this.$container.removeClass('no-scroll')
+    this.$container.removeClass("no-scroll");
 
-    window.setTimeout(() => {
-      this.$modal.removeClass('modal-enter modal-leave')
-      this.$content.removeClass('modal-content-enter modal-content-leave')
-    }, 200)
-  }
-
-  onModalClick (event) {
-    if (!this.isStaticModal() && this.$modal.is(event.target)) {
-      this.hideModal()
+    if (typeof window != "undefined") {
+      window.setTimeout(() => {
+        this.$modal.removeClass("modal-enter modal-leave");
+        this.$content.removeClass("modal-content-enter modal-content-leave");
+      }, 200);
     }
   }
 
-  fillModal () {
-    this.$content.find('.modal-body').append(this.$element.html())
+  onModalClick(event) {
+    if (!this.isStaticModal() && this.$modal.is(event.target)) {
+      this.hideModal();
+    }
   }
 
-  createModal () {
-    this.$modal = $(templates.modal)
-    this.$content = $(templates.content)
-    this.$close = $(templates.close)
+  fillModal() {
+    this.$content.find(".modal-body").append(this.$element.html());
+  }
 
-    this.$content.addClass(this.setupSizeModal(this.options.size))
+  createModal() {
+    this.$modal = $(templates.modal);
+    this.$content = $(templates.content);
+    this.$close = $(templates.close);
+
+    this.$content.addClass(this.setupSizeModal(this.options.size));
 
     if (!this.isStaticModal()) {
-      this.$content.append(this.$close)
+      this.$content.append(this.$close);
     }
 
-    this.$modal.append(this.$content)
+    this.$modal.append(this.$content);
 
-    this.$container.append(this.$modal)
+    this.$container.append(this.$modal);
 
-    this.bindTrigger()
-    this.fillModal()
+    this.bindTrigger();
+    this.fillModal();
   }
 
-  isStaticModal () {
-    return this.options.static
+  isStaticModal() {
+    return this.options.static;
   }
 
-  setupSizeModal (size) {
+  setupSizeModal(size) {
     var sizes = {
-      'small': 'modal-content-sm',
-      'medium': 'modal-content-md',
-      'large': 'modal-content-lg'
-    }
+      small: "modal-content-sm",
+      medium: "modal-content-md",
+      large: "modal-content-lg"
+    };
 
-    return sizes[size]
+    return sizes[size];
   }
 }
 
 /* istanbul ignore next */
 if ($.fn) {
-  $.fn[NAME] = function (options) {
-    options = options || {}
+  $.fn[NAME] = function(options) {
+    options = options || {};
 
-    return this.each(function () {
+    return this.each(function() {
       if (!$.data(this, NAME)) {
-        $.data(this, NAME, new Modal(this, options).init())
+        $.data(this, NAME, new Modal(this, options).init());
       }
-    })
-  }
+    });
+  };
 }
 
-export default Modal
+export default Modal;
